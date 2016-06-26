b4181bc8964c033faa343f2bc072cd1253568f08
yogeesh <yogeesh93@gmail.com>
2016-06-26 18:54:15 -0500

    6th mertics

--- security_evaluation.py
+++ security_evaluation.py
@@ -11,15 +11,24 @@ import adb_manager
 
 class rule_check:
 
-    __slots__ = "adb", "LOW_SEC", "MEDIUM_SEC", "HIGH_SEC"
-
+    __slots__ = "adb", "LOW_SEC", "MEDIUM_SEC", "HIGH_SEC",\
+                "security_level_monitor"
+    
     def __init__(self):
-        self.LOW_SEC = 1/3
-        self.MEDIUM_SEC = 2/3
-        self.HIGH_SEC = 3/3
+        self.LOW_SEC = 1/3.0 * 100
+        self.MEDIUM_SEC = 2/3.0 * 100
+        self.HIGH_SEC = 3/3.0 * 100
+        self.security_level_monitor = list()
         self.adb = adb_manager.adb()
         self.execute_rules()
+        #print self.security_level_monitor
 
+        result = self.eval_security()
+        print "\n```````````````````````"
+        print "Security level: " + str(result) + "%\n"
+        print "\nThanks :)\n\n"
+	
+        
 
     def execute_rules(self):
         """
@@ -31,16 +40,13 @@ class rule_check:
         
         self.check_usb_debugging()
         self.adb._start()
+        self.check_app_debug()
         self.check_rooted_android()
         self.check_phone_locked()
         self.check_lock_strength()
         self.check_security_patch()
-        self.check_app_debug()
         
-        print "\n```````````````````````"
-        print "Security level: \n"
-        print "\nThanks :)\n\n"
-	
+        
     def check_usb_debugging(self):
 	print "\nChecking if USB Debugging is enabled:"
         print "\tPlease disconnect other devices which are not included in security evaluation!"
@@ -51,8 +57,10 @@ class rule_check:
         
         if self.adb.get_devices() == []:
             print "\tusb debugging not enabled: Highly security\n"
+            self.security_level_monitor.append(self.HIGH_SEC)
         else:
             print "\tusb debugging enabled: Medium Security\n"
+            self.security_level_monitor.append(self.MEDIUM_SEC)
 
         
     def check_rooted_android(self):
@@ -63,10 +71,12 @@ class rule_check:
         print "\nChecking if the phone is rooted:"
         if self.adb.check_root_premission():
             print "\tAndriod phone is rooted: low security"
+            self.security_level_monitor.append(self.LOW_SEC)
         else:
             print "\tAndroid phone not rooted: high security"
+            self.security_level_monitor.append(self.HIGH_SEC)
 
-
+            
     def check_phone_locked(self):
         print "\nChecking if the phone is locked with passcode:"
         self.adb.run_command("input keyevent 26")
@@ -83,9 +93,12 @@ class rule_check:
         
         if pass_type == 3:
             print "\tAndroid phone lck screen security: low"
+            self.security_level_monitor.append(self.LOW_SEC)
         elif pass_type == 2:
             print "\tAndroid phone lock screen security: high"
+            self.security_level_monitor.append(self.HIGH_SEC)
         else:
+            self.security_level_monitor.append(self.HIGH_SEC)
             print "\tAndroid phone lock screen security: high"
 
     def check_lock_strength(self):
@@ -100,10 +113,13 @@ class rule_check:
         #TODO give an example for pattern passcode
         if len(passcode) >= 15:
             print "\tAndroid phone lock screen security: high"
+            self.security_level_monitor.append(self.HIGH_SEC)
         elif len(passcode) >= 8:
-            print "\tAndroid phone lock screen security: medium" 
+            print "\tAndroid phone lock screen security: medium"
+            self.security_level_monitor.append(self.MEDIUM_SEC)
         else:
-            print "\tAndroid phone lock screen security: low" 
+            print "\tAndroid phone lock screen security: low"
+            self.security_level_monitor.append(self.LOW_SEC)
 
 
     def check_security_patch(self):
@@ -133,11 +149,14 @@ class rule_check:
             
         #evaluation of security
         if diff_month <= 2:
-            print "Android almost upto date: high security"
+            print "\tAndroid almost upto date: high security"
+            self.security_level_monitor.append(self.HIGH_SEC)
         elif diff_month <= 6:
-            print "Android was not updated for few months: medium security"
+            print "\tAndroid was not updated for few months: medium security"
+            self.security_level_monitor.append(self.MEDIUM_SEC)
         else:
-            print "Android was not updates for a long time: low security"
+            print "\tAndroid was not updates for a long time: low security"
+            self.security_level_monitor.append(self.LOW_SEC)
 
 
     def check_app_debug(self):
@@ -151,27 +170,63 @@ class rule_check:
         if not status:
             print "\tCheck if phone is connected properly and re-run"
             return
-
         
         output = output.strip().replace("package:", "").split("\n")
         total_app = len(output)
         print "\tDetected %s installed applications", total_app
 
-        app_debuggable = 0
+        app_debuggable = []
+        app_debuggable_count = 0
         for app in output:
             print "\tTesting app: %s"%app
             status, output = self.adb.run_command("echo 'pwd' | run-as %s"%app)
             if "data" in output:
                 print "\t\tapp debuggable: low security"
-                app_debuggable += 1
+                #self.security_level_monitor.append(self.LOW_SEC)
+                app_debuggable.append(app)
+                app_debuggable_count += 1
             else:
                 print "\t\tapp not debuggable: high security"
-                pass
+                #self.security_level_monitor.append(self.HIGH_SEC)
+        
+        print "\t\nTotal debugge mode enabled applications: %s"%app_debuggable_count
+        print "\tApplication with debug mode enabled: "
+        for debug_app in app_debuggable:
+            print "\t\t%s"%debug_app
 
-        print "\t\nTotal debugge mode enabled applications: %s"%app_debuggable
 
+        if app_debuggable_count == 0:
+            self.security_level_monitor.append(HIGH_SEC)
+        elif app_debuggable_count >= total_app*0.1:
+            self.security_level_monitor.append(self.MEDIUM_SEC)
+        else:
+            self.security_level_monitor.append(self.LOW_SEC)
+            
+            
+
+    def secure_adb(self):
+        """
+        Checking if adb is running in secure mode.
+        If not running in secure mode, phone can be connected to Debug Bridge 
+        without acception RSA fingerprint check and adb can be acessed while in boot 
+        and in recovery mode/boot.
+        """
+        #TODO
+        pass
 
         
+    def eval_security(self):
+        """
+        Weighted average of the all the securtity ratings
+        """
+        weighted_sum = 0
+        weighted_total = len(self.security_level_monitor)*100
+        for sec_rating in self.security_level_monitor:
+            weighted_sum += sec_rating
+
+        return (weighted_sum/weighted_total)*100
+
+    
 if __name__ == "__main__":
     rule_verify = rule_check()
     #rule_verify.check_security_patch()
